1e61d1a6dfe2d96372ec52e8cc770115875af692
infelyor <takunyann0@gmail.com>
2020-11-24 10:04:10 +0900

    update file 11/8

--- app/src/main/java/com/example/test/MainActivity.java
+++ app/src/main/java/com/example/test/MainActivity.java
@@ -1,16 +1,29 @@
 package com.example.test;
 
+import android.os.Bundle;
+import android.widget.TextView;
+
 import androidx.appcompat.app.AppCompatActivity;
 
-import android.os.Bundle;
+import java.io.BufferedWriter;
+import java.io.ByteArrayInputStream;
+import java.io.File;
+import java.io.FileWriter;
+import java.io.IOException;
 
 public class MainActivity extends AppCompatActivity {
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        //文字入力
+        TextView textView = (TextView) findViewById(R.id.test);
+        textView.setText("こんにちは！！");
     }
 }
 
 //TEST
+//HI!
